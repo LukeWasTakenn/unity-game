f4da9264937da7294e15ce2aa5fd5f5befb92750
Luke <39926192+LukeWasTakenn@users.noreply.github.com>
2025-01-02 13:46:18 +0100

    feat: wine collecting

---
+++ Assets/Scripts/Wine.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Wine : MonoBehaviour, ICollectible
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.gameObject.CompareTag("Player")) return;
+        Collect();
+    }
+
+    public void Collect()
+    {
+        Destroy(gameObject);
+        GameController.Instance.CollectWine();
+    }
+}
