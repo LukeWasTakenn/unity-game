a201773330cc543edc46b852dcabb94f859b6fd8
Luke <39926192+LukeWasTakenn@users.noreply.github.com>
2025-01-03 16:01:30 +0100

    add level 2 and level 3

--- Assets/Scripts/Wine.cs
+++ Assets/Scripts/Wine.cs
@@ -2,6 +2,22 @@ using UnityEngine;
 
 public class Wine : MonoBehaviour, ICollectible
 {
+    public float bobSpeed = 5f;
+    public float bobHeight = 0.5f;
+
+    private Vector2 pos;
+    
+    private void Start()
+    {
+        pos = transform.position;
+    }
+    
+    private void Update()
+    {
+        float newY = Mathf.Sin(Time.time * bobSpeed) * bobHeight + pos.y;
+        transform.position = new Vector3(transform.position.x, newY, transform.position.z) ;
+    }
+    
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (!collision.gameObject.CompareTag("Player")) return;
