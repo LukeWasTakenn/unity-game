b0c609d9ea44a9be9609fbd42bffad0d11c69b78
Luke <39926192+LukeWasTakenn@users.noreply.github.com>
2025-01-06 15:08:50 +0100

    add volume sliders on main menu

--- Assets/Scripts/UI/MainMenuEvents.cs
+++ Assets/Scripts/UI/MainMenuEvents.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 
 public class MainMenuEvents : MonoBehaviour
 {
+    public AudioMixer mixer;
+    
     private UIDocument document;
 
     private void Awake()
@@ -13,26 +16,38 @@ public class MainMenuEvents : MonoBehaviour
         var playButton = document.rootVisualElement.Q<Button>("PlayButton");
         playButton.clicked += PlayButtonClicked;
         
-        var settingsButton = document.rootVisualElement.Q<Button>("SettingsButton");
-        settingsButton.clicked += SettingsButtonClicked;
-        
         var quitButton = document.rootVisualElement.Q<Button>("QuitButton");
         quitButton.clicked += QuitButtonClicked;
 
+        var sfxSlider = document.rootVisualElement.Q<Slider>("SfxSlider");
+        var musicSlider = document.rootVisualElement.Q<Slider>("MusicSlider");
+        
+        mixer.GetFloat("sfx", out float sfxValue);
+        mixer.GetFloat("music", out float musicValue);
+        sfxSlider.value = Mathf.Pow(10, sfxValue / 20);
+        musicSlider.value = Mathf.Pow(10, musicValue / 20);
+        
+        sfxSlider.RegisterCallback<ChangeEvent<float>>(SfxChanged);
+        musicSlider.RegisterCallback<ChangeEvent<float>>(MusicChanged);
     }
 
+    private void SfxChanged(ChangeEvent<float> e)
+    {
+        mixer.SetFloat("sfx", Mathf.Log10(e.newValue) * 20);
+    }
+
+    private void MusicChanged(ChangeEvent<float> e)
+    {
+        mixer.SetFloat("music", Mathf.Log10(e.newValue) * 20);        
+    }
+    
     private void PlayButtonClicked()
     {
         GameManager.ResetStopwatch();
         GameManager.StartStopwatch();
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
-
-    private void SettingsButtonClicked()
-    {
-        //
-    }
-
+    
     private void QuitButtonClicked()
     {
         Application.Quit();
