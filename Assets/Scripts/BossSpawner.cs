f16b5c3f8b3ac7288ac7d614d7511f2d07f37ac7
Luke <39926192+LukeWasTakenn@users.noreply.github.com>
2025-01-06 16:43:15 +0100

    add background music

--- Assets/Scripts/BossSpawner.cs
+++ Assets/Scripts/BossSpawner.cs
@@ -1,15 +1,28 @@
+using System;
+using SimpleAudioManager;
 using UnityEngine;
 
 public class BossSpawner : MonoBehaviour
 {
    public GameObject bossPrefab;
    public GameObject bossHealthBar;
-   
+   private GameObject songPlayerObject;
+
+   private void Awake()
+   {
+      songPlayerObject = GameObject.FindGameObjectWithTag("AudioManager");
+   }
+
+
    private void OnTriggerEnter2D(Collider2D collision)
    {
       if (!collision.gameObject.CompareTag("Player")) return;
       bossPrefab.SetActive(true);
       bossHealthBar.SetActive(true);
+      
+      var songPlayer = songPlayerObject.GetComponent<Manager>();
+      songPlayer.SetIntensity(2, 3f, 3f);
+      
       Destroy(gameObject);
    }
 }
